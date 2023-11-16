1bbf4c8c3ffedd03bd55d46f997ab9ac65120767
lamnaourm <mohammed.lamnaour@gmail.com>
2023-11-16 10:38:15 +0100

    Corretion Atelier15

---
+++ src/components/ListDepense.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ListDepense() {
+  return (
+    <div>ListDepense</div>
+  )
+}
---
+++ src/components/StatsDepenses.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function StatsDepenses() {
+  return (
+    <div>StatsDepenses</div>
+  )
+}
